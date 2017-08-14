5b7412e35345c25eda39e1269b008e523e857230
rriese <rafael.riese@catolicasc.org.br>
2017-08-13 21:33:05 -0300

    Versão Online
    Adicionado projeto web utilizando EXT JS

---
+++ extjs/resources/js/functions.js
@@ -0,0 +1,33 @@
+function getBinaryFromDecimal(number) {
+    var divisao, binary = '';
+    //var number = 10;
+
+    divisao = number / 2;
+	
+	if (divisao > 0) {
+		if (Number.isInteger(divisao)) {
+			binary = '0';
+		} else {
+			binary = '1';
+		}
+        binary = getBinaryFromDecimal(Math.trunc(divisao)) + binary;
+		console.log(binary);
+		return binary;
+    } else {
+        return '';
+    }
+}
+
+function getOctalFromDecimal(number) {
+	var divisao, octal = '', resto;
+
+    divisao = Math.trunc(number / 8);
+	resto = Math.trunc(number) - (Math.trunc(divisao * 8));
+	octal = Math.trunc(resto);
+	if (divisao > 8) {
+		octal = getOctalFromDecimal(Math.trunc(divisao)) + octal;
+		return octal;
+	} else {
+		return '';
+	} 
+}
